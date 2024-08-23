ef77bd27a776e2ce61d546a426ba9a1689a2fc1b
abrilbc <150027058+abrilbc@users.noreply.github.com>
2024-08-23 13:10:55 -0300

    shi

--- TP2/Vista/ej-1.php
+++ TP2/Vista/ej-1.php
@@ -7,7 +7,8 @@
     <title>Ejercicio 1 - TP 1</title>
     <link rel="stylesheet" href="assets/css/estilos.css">
     <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <script src="../Utils/JQuery/jquery-3.7.1.min.js"></script>
+    <script src="..\Utils\JQuery\jquery-validation-1.19.1\dist\jquery.validate.min.js"></script>
+    <script src="assets/js/form_validation"></script>
 </head>
 <body>
     <div class="header"></div>
@@ -18,9 +19,18 @@
         Investigue y pruebe la validación de formularios usando alguna librería o framework
         javaScript (JQuery, Mootools, Dojo, Prototype, etc).
         </p>
-        <form class="centrar" action="./Action/action_ej1.php" method="post" id="miFormulario">
-            <label for="number">Ingrese el número a comparar </label>
-            <input type="number" id="number" name="number" class="input-text" required>
+        <h3>INFORMACIÓN PERSONAL</h3>
+        <form class="centrar" action="" method="post" name="miForm" id="miForm">
+            <label for="nombre">Nombre: </label>
+            <input type="text" id="nombre" name="nombre" class="form-input">
+            <label for="apellido">Apellido: </label>
+            <input type="text" name="apellido" id="apellido" class="form-input">
+            <label for="email">Email: </label>
+            <input type="email" name="email" id="email" class="form-input" placeholder="john@doe.com">
+            <label for="edad">Edad: </label>
+            <input type="number" name="edad" id="edad" class="form-input">
+            <label for="contrasenia">Password</label>
+            <input type="password" name="contrasenia" class="form-input" id="contrasenia" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"/>
             <input type="submit" class="btn">
         </form>
         </div>
