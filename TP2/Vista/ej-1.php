236563a2707170db62783f603a9bb6e1bfea3623
abrilbc <150027058+abrilbc@users.noreply.github.com>
2024-08-23 12:06:36 -0300

    aaaaaa

--- TP2/Vista/ej-1.php
+++ TP2/Vista/ej-1.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 1 - TP 1</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
     <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <script src="TP1/Utils/JQuery/jquery-3.7.1.min.js"></script>
+    <script src="../Utils/JQuery/jquery-3.7.1.min.js"></script>
 </head>
 <body>
     <div class="header"></div>
@@ -15,11 +15,10 @@
         <div class="centrar">
         <h2>CONSIGNA</h2>
         <p class="texto-normal">
-        Confeccionar un formulario que solicite un número. Al pulsar el botón de enviar debe llamar a un script 
-        –vernumero.php- y visualizar un mensaje que indique si el número enviado fue: positivo, cero o negativo. 
-        Añadir un link, a la página que visualiza la respuesta, que permita volver a la página anterior.
+        Investigue y pruebe la validación de formularios usando alguna librería o framework
+        javaScript (JQuery, Mootools, Dojo, Prototype, etc).
         </p>
-        <form action="./Action/action_ej1.php" method="post" id="miFormulario">
+        <form class="centrar" action="./Action/action_ej1.php" method="post" id="miFormulario">
             <label for="number">Ingrese el número a comparar </label>
             <input type="number" id="number" name="number" class="input-text" required>
             <input type="submit" class="btn">
---
+++ TP2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios</title>
+    <link rel="stylesheet" href="Vista/css/estilos.css">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header"></div>    
+    <div class="container">
+    <ul>
+        <li><a href="./Vista/ej-1.php">TP 2 - Ejercicio 1</a></li>
+        <li><a href="./Vista/Ejercicio2.php">TP 1 - Ejercicio 2</a></li>
+        <li><a href="./Vista/Ejercicio3.php">TP 1 - Ejercicio 3</a></li>
+        <li><a href="./Vista/Ejercicio4.php">TP 1 - Ejercicio 4</a></li>
+        <li><a href="./Vista/Ejercicio5.php">TP 1 - Ejercicio 5</a></li>
+        <li><a href="./Vista/Ejercicio6.php">TP 1 - Ejercicio 6</a></li>
+        <li><a href="./Vista/Ejercicio7.php">TP 1 - Ejercicio 7</a></li>
+        <li><a href="./Vista/Ejercicio8.php">TP 1 - Ejercicio 8</a></li>
+    </ul>
+    </div>
+</body>
+</html>
