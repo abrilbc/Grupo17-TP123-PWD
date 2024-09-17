24f8e81855f5465023f3159330ceed506c81f56e
abrilbc <150027058+abrilbc@users.noreply.github.com>
2024-09-16 22:05:57 -0300

    o

--- TP4/Vista/Action/actionBuscarPersona.php
+++ TP4/Vista/Action/actionBuscarPersona.php
@@ -3,8 +3,8 @@ require_once('../../../configuracion.php');
 include_once '../Estructura/header.php';
 ?>
 <main class="container my-5">
-    <div class="p-4 border rounded-3 shadow-sm d-flex flex-column align-items-center">
-        <h3 class="text-center mb-4"><<< Persona Encontrada >>></h3>
+    <div class="bg-light p-4 border rounded-3 shadow d-flex flex-column align-items-center">
+        <h3 class="text-center mb-4">Persona Encontrada</h3>
         
         <?php
         include_once('../../Model/Connector/BaseDatos.php');
@@ -24,37 +24,37 @@ include_once '../Estructura/header.php';
 
                 $rpta = '';
                 $rpta = <<<HTML
-                    <h4 class="mb-4">Buscar y Actualizar Persona</h4>
-                    <form onsubmit="return validar()" action="ActualizarDatosPersona.php" class="d-flex flex-column gap-3 w-75" method="POST">
+                    <h4 class="mb-4">ACTUALIZAR PERSONA</h4>
+                    <form onsubmit="return validar()" action="ActualizarDatosPersona.php" class="d-flex flex-column align-items-center gap-3 w-75" method="POST">
                         <input type="hidden" id="NroDni" name="NroDni" value="{$verificacionPersona->getNroDni()}">           
-                        <div class="form-group">
+                        <div class="form-group w-100 text-center">
                             <label for="nombre" class="fs-5 fw-bold">Nombre</label>
-                            <input class="form-control p-2 mb-3 fs-6 bg-light" type="text" id="Nombre" name="Nombre" placeholder="Nombre" value="{$verificacionPersona->getNombre()}">
+                            <input class="form-control p-2 mb-3 fs-6 bg-white mx-auto w-75" type="text" id="Nombre" name="Nombre" placeholder="Nombre" value="{$verificacionPersona->getNombre()}">
                             <span class="text-danger" id="actNombre"></span>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group w-100 text-center">
                             <label for="apellido" class="fs-5 fw-bold">Apellido</label>
-                            <input class="form-control p-2 mb-3 fs-6 bg-light" type="text" id="Apellido" name="Apellido" placeholder="Apellido" value="{$verificacionPersona->getApellido()}">
+                            <input class="form-control p-2 mb-3 fs-6 bg-white mx-auto w-75" type="text" id="Apellido" name="Apellido" placeholder="Apellido" value="{$verificacionPersona->getApellido()}">
                             <span class="text-danger" id="actApellido"></span>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group w-100 text-center">
                             <label for="fechaNac" class="fs-5 fw-bold">Fecha Nacimiento</label>
-                            <input class="form-control p-2 mb-3 fs-6 bg-light" type="text" id="fechaNac" name="fechaNac" placeholder="Fecha Nacimiento" value="{$fechaFormateada}">
+                            <input class="form-control p-2 mb-3 fs-6 bg-white mx-auto w-75" type="text" id="fechaNac" name="fechaNac" placeholder="Fecha Nacimiento" value="{$fechaFormateada}">
                             <span class="text-danger" id="actFecha"></span>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group w-100 text-center">
                             <label for="telefono" class="fs-5 fw-bold">Teléfono</label>
-                            <input class="form-control p-2 mb-3 fs-6 bg-light" type="text" id="Telefono" name="Telefono" placeholder="Teléfono" value="{$verificacionPersona->getTel()}">
+                            <input class="form-control p-2 mb-3 fs-6 bg-white mx-auto w-75" type="text" id="Telefono" name="Telefono" placeholder="Teléfono" value="{$verificacionPersona->getTel()}">
                             <span class="text-danger" id="actTel"></span>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group w-100 text-center">
                             <label for="domicilio" class="fs-5 fw-bold">Domicilio</label>
-                            <input class="form-control p-2 mb-3 fs-6 bg-light" type="text" id="Domicilio" name="Domicilio" placeholder="Domicilio" value="{$verificacionPersona->getDomicilio()}">
+                            <input class="form-control p-2 mb-3 fs-6 bg-white mx-auto w-75" type="text" id="Domicilio" name="Domicilio" placeholder="Domicilio" value="{$verificacionPersona->getDomicilio()}">
                             <span class="text-danger" id="actDom"></span>
                         </div>
-                        <div class="d-flex flex-column gap-3 w-100">
-                            <button class="btn btn-success fs-5" type="submit">Actualizar</button>
-                            <a onclick="history.back()" class="btn btn-secondary fs-5" style="text-align: center;">Volver</a>
+                        <div class="d-flex flex-column align-items-center gap-3 w-100">
+                            <button class="btn btn-success fs-5 w-50" type="submit">Actualizar</button>
+                            <a onclick="history.back()" class="btn btn-secondary fs-5 w-50 text-center" style="text-decoration: none;">Volver</a>
                         </div>
                     </form>
                 HTML;
@@ -72,3 +72,4 @@ include_once '../Estructura/header.php';
 <script src="../Js/validarActPersona.js"></script>
 </body>
 </html>
+
