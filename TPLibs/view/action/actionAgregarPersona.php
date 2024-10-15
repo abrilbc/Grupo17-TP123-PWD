204a9896b844d32b29f74c3909beca88a3f4ecea
Brisa Celayes <brisa.celayes@est.fi.uncoma.edu.ar>
2024-10-15 13:44:30 -0300

    ye

--- TPLibs/view/action/actionAgregarPersona.php
+++ TPLibs/view/action/actionAgregarPersona.php
@@ -9,14 +9,21 @@ use controller\AbmPersona;
 $objAbmPersona = new AbmPersona();
 $msj = '';
 $msjTipo = '';
+$personaAgregada = null;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $datos = darDatosSubmitted();
+    
 
     $resp = $objAbmPersona->agregarPersona();
     if ($resp === 'Éxito') {
         $msj = 'Persona agregada con éxito.';
         $msjTipo = 'success';
+        $persona = new AbmPersona();
+        $datosPersona = $persona->buscarPersona($datos['legajo']);
+        $personaAgregada = $datosPersona; // Guardamos los datos de la persona 
+        $carrera = $personaAgregada->getObjCarrera();
+        $rol = $personaAgregada->getObjRol();
     } elseif ($resp === 'Error') {
         $msj = 'Ya existe alguien con este legajo.';
         $msjTipo = 'danger';
@@ -38,10 +45,23 @@ include_once '../Estructura/header.php';
         </div>
     <?php endif; ?>
 
+    <?php if ($personaAgregada): ?>
+        <div class="card mt-4">
+            <div class="card-body">
+                <h5 class="card-title">Datos de la Persona Agregada</h5>
+                <p><strong>Legajo:</strong> <?php echo htmlspecialchars($personaAgregada->getLegajo()); ?></p>
+                <p><strong>Nombre y Apellido:</strong> <?php echo htmlspecialchars($personaAgregada->getNombre()); ?></p>
+                <p><strong>Carrera:</strong> <?php echo htmlspecialchars($carrera->getNombre()); ?></p>
+                <p><strong>Rol:</strong> <?php echo htmlspecialchars($rol->getNombre()); ?></p>
+            </div>
+        </div>
+    <?php endif; ?>
+
     <div class="d-flex justify-content-center">
         <a href="../agregarPersona.php" class="btn btn-secondary btn-lg mt-3">Volver</a>
     </div>
 </div>
+
 <?php
 include_once '../../../Vista/Estructura/footer.php';
 ?>
