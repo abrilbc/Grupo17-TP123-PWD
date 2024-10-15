a52340369da7de4866ec1e41532ef9ca9abf4e3a
Brisa Celayes <brisa.celayes@est.fi.uncoma.edu.ar>
2024-10-15 14:09:15 -0300

    colores otra vez

--- TPLibs/index.php
+++ TPLibs/index.php
@@ -12,8 +12,8 @@ include_once './view/Estructura/header.php';
                     <h5 class="card-title">Gestión de Personas</h5>
                     <p class="card-text">Administra las personas registradas en el sistema.</p>
                     <a href="./view/agregarPersona.php" class="btn btn-success mb-2 w-100">Agregar Persona</a>
-                    <a href="./view/listarPersonas.php" class="btn btn-primary mb-2 w-100">Listar Personas</a>
-                    <a href="./view/buscarPersona.php" class="btn btn-info w-100">Buscar Persona</a>
+                    <a href="./view/listarPersonas.php" class="btn btn-white mb-2 w-100 border border-2">Listar Personas</a>
+                    <a href="./view/buscarPersona.php" class="btn btn-success w-100">Buscar Persona</a>
                 </div>
             </div>
         </div>
@@ -24,8 +24,8 @@ include_once './view/Estructura/header.php';
                     <h5 class="card-title">Gestión de Roles</h5>
                     <p class="card-text">Administra los roles disponibles en el sistema.</p>
                     <a href="./view/agregarRol.php" class="btn btn-success mb-2 w-100">Agregar Rol</a>
-                    <a href="./view/listarRoles.php" class="btn btn-primary mb-2 w-100">Listar Roles</a>
-                    <a href="./view/buscarRol.php" class="btn btn-info w-100">Buscar Rol</a>
+                    <a href="./view/listarRoles.php" class="btn btn-white mb-2 w-100 border border-2">Listar Roles</a>
+                    <a href="./view/buscarRol.php" class="btn btn-success w-100">Buscar Rol</a>
                 </div>
             </div>
         </div>
@@ -36,15 +36,15 @@ include_once './view/Estructura/header.php';
                     <h5 class="card-title">Gestión de Carreras</h5>
                     <p class="card-text">Administra las carreras registradas en el sistema.</p>
                     <a href="./view/agregarCarrera.php" class="btn btn-success mb-2 w-100">Agregar Carrera</a>
-                    <a href="./view/listarCarreras.php" class="btn btn-primary mb-2 w-100">Listar Carreras</a>
-                    <a href="./view/buscarCarrera.php" class="btn btn-info w-100">Buscar Carrera</a>
+                    <a href="./view/listarCarreras.php" class="btn btn-white mb-2 w-100 border border-2">Listar Carreras</a>
+                    <a href="./view/buscarCarrera.php" class="btn btn-success w-100">Buscar Carrera</a>
                 </div>
             </div>
         </div>
 
     </div>
     <div class="d-flex justify-content-center mt-4">
-        <a href="../" class="btn btn-secondary fs-5">Volver</a>
+        <a href="../" class="btn btn-secondary fs-6">Volver al Inicio</a>
     </div>
 </div>
 <?php
